0d1b427b23130f9c5c245a48b01d3767aa23bb8c
SourScar <sourscar@outlook.com>
2021-05-28 22:55:59 +0500

    TweentySix

--- index.js
+++ index.js
@@ -38,11 +38,7 @@ io.on('connection', socket => {
         io.emit('buttonStop', 'stopCount');
     });
     socket.on('timer', message => {
-        if(message == 00+'-'+00){
-            socket.disconnect();
-        }else{
-            io.emit('timerTime', message);
-        }
+        io.emit('timerTime', message);
     });
 });
 // API request
